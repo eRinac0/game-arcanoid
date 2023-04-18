ac99e43ce8c9b8f7fbc55d6594739e8c461bb2a6
Vladik <vladkramarenko53@gmail.com>
2023-04-18 20:38:43 +0300

    version 1.1

--- game.js
+++ game.js
@@ -1,35 +1,158 @@
-var game ={
-    ctx:undefined,
-    sprites:{
-        background:undefined,
-        platform:undefined,
-    },
-    start: function(){
-        var canvas=document.getElementById("mygame");
-        this.ctx=canvas.getContext("2d");
-        this.sprites.background = new Image();
-        this.sprites.background.src = "./css/img/background.jpg";
-        this.sprites.platform = new Image();
-        this.sprites.platform.src = "./css/img/platform.png";
-        this.run();
-    
-    },
-        render: function() {
-        this.ctx.drawImage(this.sprites.background,0,0);
-        this.ctx.drawImage(this.sprites.platform ,0,0);
-
-        },
-        run: function(){
-            this.render();
-window.requestAnimationFrame(function(){
-game.run();
-        });
-        }
-};
-game.platform={
-    
+let canvas = document.getElementById("mygame");
+let ctx = canvas.getContext("2d");
+let ballRadius = 10;
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+let score =0;
+let brickRowCount = 5;
+let brickColumnCount = 3;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+let colorball ="#0095DD";
+//масив который отвечает за "сетку" блоков 
+let bricks = [];
+for(let c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
 }
 
-window.addEventListener("load",function(){
-    game.start();
-})
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+//управление кнопками
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+//отрисовка мяча
+function drawBall( colorball) {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = colorball;
+    ctx.fill();
+    ctx.closePath();
+}
+//отрисовка платформы
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+
+//отрисовка 
+function drawBricks() {
+    for(let c=0; c<brickColumnCount; c++) {
+      for(let r=0; r<brickRowCount; r++) {
+        if(bricks[c][r].status == 1) {
+          let brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+          let brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+          bricks[c][r].x = brickX;
+          bricks[c][r].y = brickY;
+          ctx.beginPath();
+          ctx.rect(brickX, brickY, brickWidth, brickHeight);
+          ctx.fillStyle = "#0095DD";
+          ctx.fill();
+          ctx.closePath();
+        }
+      }
+    }
+  }
+  function generateColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16)
+  }
+  function collisionDetection() {
+    for(let c=0; c<brickColumnCount; c++) {
+      for(let r=0; r<brickRowCount; r++) {
+        let b = bricks[c][r];
+        if(b.status == 1) {
+          if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+            dy = -dy;
+            b.status = 0;
+            score++;
+            colorball=generateColor();
+            if(score == brickRowCount*brickColumnCount) {
+                alert("YOU WIN, CONGRATULATIONS!");
+                document.location.reload();
+          }
+        }
+        }
+      }
+    }
+  }
+
+function generateColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16)
+}
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+}
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBricks();
+
+    drawBall(colorball);
+    drawPaddle();
+    collisionDetection();
+    drawScore();
+
+    //отбивание от стенок и окончание игры
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy*1.1;
+        }
+        else {
+            
+            alert(`GAME OVER \nRESULT: ${score}`);
+            document.location.reload();
+        }
+    }
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+    
+    x += dx;
+    y += dy;
+    requestAnimationFrame(draw);
+
+}
+
+draw();
